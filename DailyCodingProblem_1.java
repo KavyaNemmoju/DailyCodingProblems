03b2e94c0648e89d07c48ba0f9a9c418b76401b2
KavyaNemmoju <103114183+KavyaNemmoju@users.noreply.github.com>
2022-05-06 12:59:59 +0530

    Add files via upload

---
+++ DailyCodingProblem_1.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class ListAddition {
+    public static boolean list_add(int k, int[] list){
+        for (int i=0; i<list.length; i++){
+            for (int j=i+1; j<list.length; j++){
+                if (list[i]+list[j]==k)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String [] args){
+        int[] l = {10,15,3,7};
+        System.out.println(list_add(17, l));
+    }
+}
