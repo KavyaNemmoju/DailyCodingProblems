7324ee2bee74a6f3dd6f85180cf24194d2714b87
KavyaNemmoju <103114183+KavyaNemmoju@users.noreply.github.com>
2022-06-16 14:18:04 +0530

    Update problem_properties.java

--- problem_properties.java
+++ problem_properties.java
@@ -1,27 +1,33 @@
 // printing properties of a particular file
-
-
-
-
-
 import java.io.File;
-class  Parse{
+import java.util.Scanner;
+class fileProperty{
 	public static void main(String[] args)
 	{
 		
-		String fname = "EXCEPTION HANDLING NOTES.txt";
+		Scanner sc=new Scanner(System.in);
+		String fname = sc.nextLine();
+
 		
 		File f = new File(fname);
-		
-		System.out.println("File name :" + f.getName());
-		System.out.println("Path: " + f.getPath());
-		System.out.println("Absolute path:"+ f.getAbsolutePath());
-		System.out.println("Parent:" + f.getParent());
-		System.out.println("Exists :" + f.exists());
-		if (f.exists()) {
+		String content[]=f.list();
+		for(int i=0;i<content.length;i++){
+			if(content[i].endsWith(".txt")){
+				System.out.println(content[i]);
+			}
 			
-			System.out.println("Is a directory:"+ f.isDirectory());
-			System.out.println("File Size in bytes "+ f.length());
 		}
+
+		// System.out.println("File name :" + f.getName());
+		// System.out.println("Path: " + f.getPath());
+		// System.out.println("Absolute path:"+ f.getAbsolutePath());
+		// System.out.println("Parent:" + f.getParent());
+		// System.out.println("Exists :" + f.exists());
+		// if (f.exists()) {
+			
+			
+		// 	System.out.println("Is a directory:"+ f.isDirectory());
+		// 	System.out.println("File Size in bytes "+ f.length());
+		// }
 	}
 }
