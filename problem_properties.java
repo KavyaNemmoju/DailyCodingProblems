2a10ac082b87e79c5b4c5441d3536cd3ed05ee37
KavyaNemmoju <103114183+KavyaNemmoju@users.noreply.github.com>
2022-06-17 06:57:23 +0530

    Update problem_properties.java

--- problem_properties.java
+++ problem_properties.java
@@ -1,4 +1,9 @@
 // printing properties of a particular file
+
+
+
+
+
 import java.io.File;
 import java.util.Scanner;
 class fileProperty{
@@ -10,13 +15,22 @@ class fileProperty{
 
 		
 		File f = new File(fname);
+		
+			
 		String content[]=f.list();
 		for(int i=0;i<content.length;i++){
 			if(content[i].endsWith(".txt")){
 				System.out.println(content[i]);
+				System.out.println("File Size in bytes "+ f.length());
+				System.out.println("Is a directory:"+ f.isDirectory());
+				
+
 			}
 			
 		}
+	}
+}
+
 
 		// System.out.println("File name :" + f.getName());
 		// System.out.println("Path: " + f.getPath());
@@ -25,9 +39,12 @@ class fileProperty{
 		// System.out.println("Exists :" + f.exists());
 		// if (f.exists()) {
 			
-			
+
 		// 	System.out.println("Is a directory:"+ f.isDirectory());
 		// 	System.out.println("File Size in bytes "+ f.length());
 		// }
-	}
-}
+
+
+
+
+		
